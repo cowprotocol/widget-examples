ab91bdf278ac3f540d306426f7e1097e284c5879
Anxo Rodriguez <anxolin@gmail.com>
2023-06-21 17:42:08 +0100

    chore: add webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,8 @@
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "hello";
+
+  return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "./dist",
+  },
+};
