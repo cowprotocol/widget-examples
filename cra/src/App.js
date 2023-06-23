3f70d2f4c8f1fad81aaa8a1479eb62c9d7846b0d
Anxo Rodriguez <anxolin@gmail.com>
2023-06-23 07:57:55 +0100

    Use CoW Swap widget instead of the CoW Swap Widget lib (#1)

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,10 +1,31 @@
-import { CowSwapWidget } from "./CowSwapWidget";
+import { CowSwapWidget } from "@cowprotocol/widget-react";
 
 function App() {
+  const cowSwapWidgetParams = {
+    width: 600,
+    height: 700,
+  };
+
+  const cowSwapWidgetSettings = {
+    urlParams: {
+      chainId: 1,
+      tradeType: "swap",
+      env: "local",
+      tradeAssets: {
+        sell: { asset: "WETH", amount: "0.1" },
+        buy: { asset: "DAI" },
+      },
+    },
+    appParams: {},
+  };
+
   return (
     <>
       <h1>CoW Widget - Create React App (CRA)</h1>
-      <CowSwapWidget />
+      <CowSwapWidget
+        params={cowSwapWidgetParams}
+        settings={cowSwapWidgetSettings}
+      />
     </>
   );
 }
