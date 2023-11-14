c3ff595f7c01846394c3c5faef803228a6d568e6
Anxo Rodriguez <anxolin@gmail.com>
2023-11-14 16:26:45 +0300

    Refactor layout

---
+++ classic/src/Layout/Drawer.tsx
@@ -0,0 +1,57 @@
+import MuiDrawer from "@mui/material/Drawer";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+
+import { DRAWER_WIDTH } from ".";
+
+export const StyledDrawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: DRAWER_WIDTH,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+export interface DrawerProps {
+  open: boolean;
+  toggleDrawer: () => void;
+}
+export function Drawer({ open, toggleDrawer }: DrawerProps) {
+  return (
+    <StyledDrawer variant="permanent" open={open}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          px: [1],
+        }}
+      >
+        <IconButton onClick={toggleDrawer}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </Toolbar>
+      Hello
+    </StyledDrawer>
+  );
+}
