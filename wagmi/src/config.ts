dbd403a524bc507fd94dd2b42b0980e46868e11e
shoom3301 <shoom3301@gmail.com>
2025-06-16 10:30:04 +0500

    chore: setup wagmi

---
+++ wagmi/src/config.ts
@@ -0,0 +1,32 @@
+import { QueryClient } from '@tanstack/react-query'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { http, createConfig, deserialize, serialize } from 'wagmi'
+import { injected, walletConnect } from 'wagmi/connectors'
+
+
+export const wagmiCconfig = createConfig({
+    chains: [mainnet, sepolia],
+    transports: {
+        [mainnet.id]: http(),
+        [sepolia.id]: http(),
+    },
+    connectors: [
+        injected(),
+        walletConnect({ projectId: 'cow-widge-wagmi' }),
+    ],
+})
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            gcTime: 1_000 * 60 * 60 * 24, // 24 hours
+        },
+    },
+})
+
+export const persister = createSyncStoragePersister({
+    serialize,
+    storage: window.localStorage,
+    deserialize,
+})
